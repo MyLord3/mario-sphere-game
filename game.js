db4f86904c99e9e31e1822fc4462bc302e084b19
MyLord <119475315+MyLord3@users.noreply.github.com>
2026-03-25 18:50:39 +0530

    Create game.js

---
+++ game.js
@@ -0,0 +1,61 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 500 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game;
+let player;
+let cursors;
+let platforms;
+
+function startGame() {
+    if (!game) {
+        game = new Phaser.Game(config);
+    }
+}
+
+function preload() {
+    this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
+    this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
+}
+
+function create() {
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 580, 'ground').setScale(2).refreshBody();
+
+    player = this.physics.add.sprite(100, 450, 'player');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(player, platforms);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-350);
+    }
+}
