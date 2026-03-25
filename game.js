94959f78a1b6062ab18df8914c3b3cd201988be7
MyLord <119475315+MyLord3@users.noreply.github.com>
2026-03-25 19:42:40 +0530

    Update game.js

--- game.js
+++ game.js
@@ -24,6 +24,7 @@ let platforms;
 let coins;
 let score = 0;
 let scoreText;
+let jumpCount = 0;
 
 function startGame() {
     if (!game) {
@@ -38,34 +39,37 @@ function preload() {
 }
 
 function create() {
-    // WORLD SIZE (important for scrolling)
-    this.physics.world.setBounds(0, 0, 2000, 600);
+    this.physics.world.setBounds(0, 0, 4000, 600);
 
     platforms = this.physics.add.staticGroup();
 
-    // Ground
-    for (let i = 0; i < 2000; i += 400) {
+    for (let i = 0; i < 4000; i += 400) {
         platforms.create(i, 580, 'ground').setScale(2).refreshBody();
     }
 
-    // Floating platforms
-    platforms.create(600, 400, 'ground');
-    platforms.create(900, 300, 'ground');
-    platforms.create(1300, 350, 'ground');
-    platforms.create(1600, 250, 'ground');
+    platforms.create(600, 450, 'ground');
+    platforms.create(900, 350, 'ground');
+    platforms.create(1200, 300, 'ground');
+    platforms.create(1500, 400, 'ground');
+    platforms.create(1800, 320, 'ground');
+    platforms.create(2200, 280, 'ground');
+    platforms.create(2600, 350, 'ground');
+    platforms.create(3000, 300, 'ground');
+    platforms.create(3400, 420, 'ground');
 
     player = this.physics.add.sprite(100, 450, 'player');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
-    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, platforms, function () {
+        jumpCount = 0;
+    });
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    // Coins
     coins = this.physics.add.group({
         key: 'coin',
-        repeat: 10,
+        repeat: 20,
         setXY: { x: 200, y: 0, stepX: 150 }
     });
 
@@ -79,8 +83,7 @@ function create() {
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '24px', fill: '#fff' });
     scoreText.setScrollFactor(0);
 
-    // CAMERA FOLLOW PLAYER (side scrolling)
-    this.cameras.main.setBounds(0, 0, 2000, 600);
+    this.cameras.main.setBounds(0, 0, 4000, 600);
     this.cameras.main.startFollow(player);
 }
 
@@ -92,14 +95,17 @@ function collectCoin(player, coin) {
 
 function update() {
     if (cursors.left.isDown) {
-        player.setVelocityX(-160);
+        player.setVelocityX(-200);
     } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
+        player.setVelocityX(200);
     } else {
         player.setVelocityX(0);
     }
 
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-350);
+    if (Phaser.Input.Keyboard.JustDown(cursors.up)) {
+        if (jumpCount < 2) {
+            player.setVelocityY(-400);
+            jumpCount++;
+        }
     }
 }
