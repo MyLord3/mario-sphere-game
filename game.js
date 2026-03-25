a8ce98c81bf2038669ab339dab2e4acfe1823ca4
MyLord <119475315+MyLord3@users.noreply.github.com>
2026-03-25 19:15:09 +0530

    Update game.js

--- game.js
+++ game.js
@@ -21,6 +21,9 @@ let game;
 let player;
 let cursors;
 let platforms;
+let coins;
+let score = 0;
+let scoreText;
 
 function startGame() {
     if (!game) {
@@ -31,11 +34,15 @@ function startGame() {
 function preload() {
     this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
     this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
+    this.load.image('coin', 'https://labs.phaser.io/assets/sprites/gold_1.png');
 }
 
 function create() {
     platforms = this.physics.add.staticGroup();
     platforms.create(400, 580, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 
     player = this.physics.add.sprite(100, 450, 'player');
     player.setBounce(0.2);
@@ -44,6 +51,27 @@ function create() {
     this.physics.add.collider(player, platforms);
 
     cursors = this.input.keyboard.createCursorKeys();
+
+    coins = this.physics.add.group({
+        key: 'coin',
+        repeat: 5,
+        setXY: { x: 12, y: 0, stepX: 120 }
+    });
+
+    coins.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.4));
+    });
+
+    this.physics.add.collider(coins, platforms);
+    this.physics.add.overlap(player, coins, collectCoin, null, this);
+
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '24px', fill: '#000' });
+}
+
+function collectCoin(player, coin) {
+    coin.disableBody(true, true);
+    score += 10;
+    scoreText.setText('Score: ' + score);
 }
 
 function update() {
